f91f42b7b8e20794095fe1920063fd09514fb601
nmorrone <n.morrone96@gmail.com>
2024-09-27 14:48:24 +0200

    password-generator

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -9,41 +9,60 @@ public class PasswordGenerator {
 		
 		System.out.println("Benvenuto nel tuo generatore di Password!");
 		
+		//chiedo all'utente di inserire il nome
 		Scanner userName = new Scanner(System.in);
 		
 		System.out.println("Inserisci il tuo Nome");
 
 		String name = userName.next();
 		
+		
+		//chiedo all'utente di inserire il cognome
 		Scanner userSurname = new Scanner(System.in);
 		
 		System.out.println("Adesso il tuo Cognome");
 		
 		String surname = userSurname.next();
 		
+		
+		//chiedo all'utente il colore preferito
 		Scanner favoriteColor = new Scanner(System.in);
 		
 		System.out.println("Qual è il tuo colore preferito?");
 		
 		String color = favoriteColor.next();
 		
+		
+		//chiedo all'utente il giorno di nascita
 		Scanner birthDay = new Scanner(System.in);
 		
 		System.out.println("Che giorno sei nato/a?");
 		
 		String day = birthDay.next();
 		
+		
+		//chiedo all'utente il mese di nascita
 		Scanner birthMonth = new Scanner(System.in);
 		
 		System.out.println("di quale mese?");
 		
 		String month = birthMonth.next();
 		
+		
+		//chiedo all'utente l'anno di nascita
 		Scanner birthYear = new Scanner(System.in);
 		
 		System.out.println("In che anno sei nato?");
 		
 		String year = birthYear.next();
+		
+		
+		//genero la password personalizzata dell'utente
+		
+		System.out.println("Ciao " + name + " ecco la tua password personalizzata:");
+		
+		System.out.println(name + "-" + surname + "-" + color + "-" + year);
+		
 		
 		
 	}
